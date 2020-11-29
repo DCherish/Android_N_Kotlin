0bac08bcb32654b33854a094b7ed61efb67eacf4
DCherish <jongillou@gmail.com>
2020-11-29 23:49:50 +0900

    cpp File

---
+++ D_Content/Day7/D7_1_SampleFragment/app/src/main/java/org/techtown/fragment/MenuFragment.java
@@ -0,0 +1,30 @@
+package org.techtown.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+
+        Button button2 = rootView.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity(); // 내가 어느 Activity 위에 있는지 파악
+                activity.onFragmentChanged(1);
+            }
+        });
+
+        return rootView;
+    }
+}

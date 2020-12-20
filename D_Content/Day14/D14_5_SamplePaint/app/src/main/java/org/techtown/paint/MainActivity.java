a110a91a166ddc67f0db21c1f065ad3ad9e8b610
DCherish <jongillou@gmail.com>
2020-12-20 23:17:24 +0900

    stuDy

---
+++ D_Content/Day14/D14_5_SamplePaint/app/src/main/java/org/techtown/paint/MainActivity.java
@@ -0,0 +1,20 @@
+package org.techtown.paint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*PaintBoard view = new PaintBoard(this);
+        setContentView(view);*/
+
+        BestPaintBoard view = new BestPaintBoard(this);
+        setContentView(view);
+    }
+}

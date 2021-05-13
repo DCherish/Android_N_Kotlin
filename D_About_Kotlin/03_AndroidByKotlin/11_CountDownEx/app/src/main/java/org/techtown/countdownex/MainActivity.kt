3d0cb9612dbcc775cd64fcbb1acccbce88d40198
DCherish <jongillou@gmail.com>
2021-05-13 18:57:12 +0900

    stuDy

--- D_About_Kotlin/03_AndroidByKotlin/11_CountDownEx/app/src/main/java/org/techtown/countdownex/MainActivity.kt
+++ D_About_Kotlin/03_AndroidByKotlin/11_CountDownEx/app/src/main/java/org/techtown/countdownex/MainActivity.kt
@@ -2,28 +2,80 @@ package org.techtown.countdownex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "COUNTDOWN"
+    private lateinit var job: CompletableJob
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val job = GlobalScope.launch(Dispatchers.Main, start = CoroutineStart.LAZY) {
+        if (!::job.isInitialized) {
+            initJob()
+        }
+
+        /*val job = GlobalScope.launch(Dispatchers.Main, start = CoroutineStart.LAZY) {
             for (i in 10 downTo 1) {
                 tv_message.text = "countdown $i"
                 delay(1000)
             }
             tv_message.text = "done!"
-        }
+        }*/
 
         btn_start.setOnClickListener {
-            job.start()
+            if (tv_message.text.contains("countdown")) { // running
+                // job reset
+                resetJob()
+            }
+            else {
+                // process
+                CoroutineScope(Main + job).launch {
+                    Log.d(TAG, "$job is activated: $this")
+                    for (i in 10 downTo 1) {
+                        tv_message.text = "countdown $i"
+                        delay(1000)
+                    }
+                    tv_message.text = "done!"
+                }
+            }
         }
 
         btn_stop.setOnClickListener {
-            job.cancel()
+            resetJob()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+
+    private fun resetJob() {
+        if (job.isActive || job.isCompleted) {
+            job.cancel(CancellationException("cancel job"))
+        }
+
+        initJob()
+    }
+
+    private fun initJob() {
+        job = Job()
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()) {
+                    msg = "Unknown error"
+                }
+                Log.e(TAG,"$job is completed: $msg")
+            }
+            tv_message.text = "Completed"
+        }
+        tv_message.text = "Ready"
     }
 }
